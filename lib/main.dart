ca00138bf6b2feb1c9ece92da134006ef3ccccef
m-we <mwea@protonmail.com>
2020-04-02 23:57:41 -0400

    version

--- lib/main.dart
+++ lib/main.dart
@@ -66,7 +66,7 @@ class MainState extends State<Main> {
                               width: 150
                           ),
                           Text(
-                              'v0-20200326'
+                              'v0-20200402'
                           ),
                           SizedBox(height:15),
                           TextFormField(
